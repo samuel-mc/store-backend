c2f83124025b45ba0c9f1a0ff44fd149d56bb579
Samuel Medina <smdoomed@gmail.com>
2021-11-05 20:10:08 -0600

    Deploy

--- src/db/db.sync.js
+++ src/db/db.sync.js
@@ -3,19 +3,19 @@ const { Artist } = require('../models/artist.model');
 const { Genre } = require('../models/genre.model');
 const { Song } = require('../models/song.model');
 
-// Artist.sync().then(() => {
-//     console.log('Artist table created');
-// });
+Artist.sync().then(() => {
+    console.log('Artist table created');
+});
 
-// Genre.sync().then(() => {
-//     console.log('Genre table created');
-// });
+Genre.sync().then(() => {
+    console.log('Genre table created');
+});
 
-// Album.sync().then(() => {
-//     console.log('Album table created');
-// });
+Album.sync().then(() => {
+    console.log('Album table created');
+});
 
 
-// Song.sync().then(() => {
-//     console.log('Song table created');
-// })
+Song.sync().then(() => {
+    console.log('Song table created');
+})
