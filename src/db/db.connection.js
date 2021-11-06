bc8d1f0afd3bbf2945f03d76b04b283447cd20ec
Samuel Medina <smdoomed@gmail.com>
2021-11-05 20:42:22 -0600

    [fix] db connection

--- src/db/db.connection.js
+++ src/db/db.connection.js
@@ -1,6 +1,6 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
